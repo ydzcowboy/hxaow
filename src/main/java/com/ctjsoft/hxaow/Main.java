f483d463cde94d0176ee27a377d6de25cbddf4e8
lqz001 <lqz@192.168.252.182>
2017-03-29 17:23:04 +0800

    213

--- src/main/java/com/ctjsoft/hxaow/Main.java
+++ src/main/java/com/ctjsoft/hxaow/Main.java
@@ -45,7 +45,7 @@ import com.ctjsoft.hxaow.bo.VerInfo;
 public class Main {
     private static Log LOG;
 
-    /**
+    /** lqz
      * The property name for the directory containing a list of jars to load on the classpath.
      */
     private static final String PROPERTY_JAR_DIRS = "flyway.jarDirs";
@@ -58,6 +58,7 @@ public class Main {
     static void initLogging(Level level) {
         LogFactory.setFallbackLogCreator(new ConsoleLogCreator(level));
         LOG = LogFactory.getLog(Main.class);
+        
     }
 
     /**
