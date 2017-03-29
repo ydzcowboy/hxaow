19a7ae74a4e88310053278d59989feef3abc23b4
lqz001 <longqunzhi891115@126.com>
2017-03-29 16:10:14 +0800

    Update Main.java
    123

--- src/main/java/com/ctjsoft/hxaow/Main.java
+++ src/main/java/com/ctjsoft/hxaow/Main.java
@@ -38,7 +38,7 @@ import com.ctjsoft.hxaow.bo.VerInfo;
 
 /**
  * Main class and central entry point of the hxaow command-line tool.
- * 韩信 command 基于 flyway 4.0.3  by lqz001
+ * 韩信 command 基于 flyway 4.0.3 
  * @author ydzcowboy
  *
  */
@@ -52,7 +52,7 @@ public class Main {
 
     /**
      * Initializes the logging.
-     *
+     * by lqz001
      * @param level The minimum level to log at.
      */
     static void initLogging(Level level) {
